868943c3caa5b87715c3b048a0197830d1fc6970
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-01-10 20:44:45 +0000

    Adding sidebar features

--- src/Bases/PackageServiceProvider.php
+++ src/Bases/PackageServiceProvider.php
@@ -36,6 +36,52 @@ abstract class PackageServiceProvider extends ServiceProvider
     }
 
     /* ------------------------------------------------------------------------------------------------
+     |  Sidebar Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Get the sidebar folder.
+     *
+     * @return string
+     */
+    protected function getSidebarFolder()
+    {
+        return $this->getConfigFolder() . DS . 'sidebar' . DS . $this->package;
+    }
+
+    /**
+     * Get the sidebar config key.
+     *
+     * @return string
+     */
+    protected function getSidebarKey()
+    {
+        return "$this->vendor.sidebar.$this->package";
+    }
+
+    /**
+     * Register all the sidebar config files.
+     */
+    protected function registerSidebarItems()
+    {
+        $paths = $this->filesystem()->glob($this->getSidebarFolder() . DS . '*.php');
+
+        foreach ($paths as $path) {
+            $this->mergeConfigFrom($path, $this->getSidebarKey() . '.' . basename($path, '.php'));
+        }
+    }
+
+    /**
+     * Publish all the sidebar config files.
+     */
+    protected function publishSidebarItems()
+    {
+        $this->publishes([
+            $this->getSidebarFolder() => config_path($this->vendor . DS . 'sidebar' . DS . $this->package)
+        ], 'sidebar');
+    }
+
+    /* ------------------------------------------------------------------------------------------------
      |  Other Functions
      | ------------------------------------------------------------------------------------------------
      */
@@ -47,5 +93,15 @@ abstract class PackageServiceProvider extends ServiceProvider
     protected function config()
     {
         return $this->app['config'];
+    }
+
+    /**
+     * Get the filesystem instance.
+     *
+     * @return \Illuminate\Filesystem\Filesystem
+     */
+    protected function filesystem()
+    {
+        return $this->app['files'];
     }
 }
