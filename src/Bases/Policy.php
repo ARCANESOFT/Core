93150f2ead36f30ac01b97a73148933bd3d35747
ARCANEDEV <arcanedev.maroc@gmail.com>
2017-02-23 12:45:39 +0000

    Updating the Base Policy class

--- src/Bases/Policy.php
+++ src/Bases/Policy.php
@@ -1,6 +1,7 @@
 <?php namespace Arcanesoft\Core\Bases;
 
 use Arcanesoft\Contracts\Auth\Models\User;
+use Arcanedev\Support\Bases\Policy as BasePolicy;
 
 /**
  * Class     Policy
@@ -8,13 +9,15 @@ use Arcanesoft\Contracts\Auth\Models\User;
  * @package  Arcanesoft\Core\Bases
  * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
  */
-abstract class Policy
+abstract class Policy extends BasePolicy
 {
     /* ------------------------------------------------------------------------------------------------
      |  Main Functions
      | ------------------------------------------------------------------------------------------------
      */
     /**
+     * The policy's before filter.
+     *
      * @param  \Arcanesoft\Contracts\Auth\Models\User  $user
      * @param  mixed                                   $ability
      *
