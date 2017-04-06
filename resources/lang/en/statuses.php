d1c28dcf44496dd1673bce34e82c2ef27dd0411b
ARCANEDEV <arcanedev.maroc@gmail.com>
2017-04-06 16:03:02 +0100

    Adding more translations

--- resources/lang/en/statuses.php
+++ resources/lang/en/statuses.php
@@ -1,5 +1,27 @@
 <?php
 
 return [
-    //
+    'yes'         => 'yes',
+    'no'          => 'no',
+
+    'saved'       => 'saved',
+    'created'     => 'created',
+    'updated'     => 'updated',
+    'deleted'     => 'deleted',
+    'restored'    => 'restored',
+    'expired'     => 'expired',
+
+    'enabled'     => 'enabled',
+    'activated'   => 'activated',
+    'disabled'    => 'disabled',
+    'deactivated' => 'deactivated',
+
+    'confirmed'   => 'confirmed',
+    'unconfirmed' => 'unconfirmed',
+    'locked'      => 'locked',
+    'unlocked'    => 'unlocked',
+
+    'public'      => 'public',
+    'private'     => 'private',
+    'protected'   => 'protected',
 ];
--- resources/lang/fr/statuses.php
+++ resources/lang/fr/statuses.php
@@ -1,5 +1,28 @@
 <?php
 
 return [
+    'yes'         => 'oui',
+    'no'          => 'non',
 
+    'saved'       => 'enregistré',
+    'created'     => 'créé',
+    'updated'     => 'mis à jour',
+    'deleted'     => 'supprimé',
+    'cleared'     => 'effacé',
+    'restored'    => 'restauré',
+    'expired'     => 'expiré',
+
+    'enabled'     => 'activé',
+    'activated'   => 'activé',
+    'disabled'    => 'désactivé',
+    'deactivated' => 'désactivé',
+
+    'confirmed'   => 'confirmé',
+    'unconfirmed' => 'non confirmé',
+    'locked'      => 'verrouillé',
+    'unlocked'    => 'déverrouillé',
+
+    'public'      => 'publique',
+    'private'     => 'privé',
+    'protected'   => 'protégé',
 ];
