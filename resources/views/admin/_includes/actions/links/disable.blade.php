47d59840c35b4c100ea6173def9a58f0d8300b34
ARCANEDEV <arcanedev.maroc@gmail.com>
2017-04-12 19:09:10 +0100

    Cleaning the view folder

--- resources/views/admin/_includes/actions/links/disable.blade.php
+++
@@ -1,8 +0,0 @@
-<?php
-$url       = isset($url)       ? $url       : '#';
-$linkClass = isset($linkClass) ? $linkClass : 'btn btn-sm btn-inverse';
-$iconClass = isset($iconClass) ? $iconClass : 'fa fa-fw fa-power-off';
-$linkTitle = isset($linkTitle) ? $linkClass : trans('core::actions.disable');
-$disabled  = isset($disabled)  ? $disabled  : false;
-?>
-@include('core::admin._includes.actions.links._base', compact('url', 'linkClass', 'linkTitle', 'iconClass', 'disabled'))
--- resources/views/admin/_includes/actions/links/restore.blade.php
+++
@@ -1,8 +0,0 @@
-<?php
-$url       = isset($url)       ? $url       : '#';
-$linkClass = isset($linkClass) ? $linkClass : 'btn btn-sm btn-primary';
-$iconClass = isset($iconClass) ? $iconClass : 'fa fa-fw fa-reply';
-$linkTitle = isset($linkTitle) ? $linkClass : trans('core::actions.restore');
-$disabled  = isset($disabled)  ? $disabled  : false;
-?>
-@include('core::admin._includes.actions.links._base', compact('url', 'linkClass', 'linkTitle', 'iconClass', 'disabled'))
